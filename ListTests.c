d95e871eff68c5dccdae216990316c7fe85346ac
vitalij <1889621+vitalij555@users.noreply.github.com>
2020-09-16 20:06:29 +0300

    Add tests representing motivational section in readme.md

--- ListTests.c
+++ ListTests.c
@@ -35,7 +35,8 @@ TEST_GROUP_RUNNER(List)
   {RUN_TEST_CASE(List, listPushToEnd_Ok);}
   {RUN_TEST_CASE(List, listPushFront_Ok);}
   {RUN_TEST_CASE(List, listGetFirstNext_Ok);}
-
+  {RUN_TEST_CASE(List, motivationCodeTest_Ok);}
+  {RUN_TEST_CASE(List, motivation2CodeTest_Ok);}
 }
 
 
@@ -136,3 +137,76 @@ TEST(List, listGetFirstNext_Ok)
  }
 
 
+
+TEST(List, motivationCodeTest_Ok)
+{
+	int data = 1000;
+	int data2 = 2000;
+	int* dataOut = 0;
+	int* dataOut2 = 0;
+	HList list = listCreate(sizeof(int));
+
+	listPush(&list, &data);
+	listPush(&list, &data2);
+
+	dataOut = listGetFirst(&list); 
+	dataOut2 = listGetNext(&list);  
+
+  TEST_ASSERT_EQUAL_INT(1000, *(int*)dataOut);
+  TEST_ASSERT_EQUAL_INT(2000, *(int*)dataOut2);
+
+	memoryFree((void**)&dataOut);   // Frees our first integer
+	memoryFree((void**)&dataOut2);  // Frees our second integer
+	listFree(&list); // Frees the list and all elements left in it
+
+  TEST_ASSERT_NULL(dataOut);
+  TEST_ASSERT_NULL(dataOut2);
+  TEST_ASSERT_NULL(list);
+}
+
+
+
+#define MEDIUM_STRING 64
+#define LONG_STRING 128
+// Test without assertions - just to be sure it compiles and prints results
+TEST(List, motivation2CodeTest_Ok)
+{
+    typedef struct 
+    {
+       char country[MEDIUM_STRING];
+       char city[MEDIUM_STRING];
+       char street[LONG_STRING];   
+       int houseNumber;
+       int flat; 
+       char zip[6];        
+    }Address;
+
+  Address address1 = {"Lithuania", "Vilnius", "Zirmunu", 525, 15, "09127"};
+  Address address2 = {"Czech Republic", "Praha", "Resslova", 1939, 7, "2"};
+  Address address3 = {"Belgium", "Brussels", "Atomiumplein", 1, 1, "B-1020"};
+
+
+	HList list = listCreate(sizeof(Address));
+
+	listPush(&list, &address1);
+	listPush(&list, &address2);
+  listPush(&list, &address3);
+
+  Address* addressOut = NULL;
+	addressOut = listGetFirst(&list);
+  while (addressOut)
+  {
+    printf("\n####################\n\nCountry:%s\nCity:%s\nStreet:%s\nNr.: %d-%d  %s", 
+              addressOut->country, 
+              addressOut->city, 
+              addressOut->street, 
+              addressOut->houseNumber, 
+              addressOut->flat, 
+              addressOut->zip);
+
+    memoryFree((void**)&addressOut); 
+    addressOut = listGetNext(&list);
+  }
+
+ listFree(&list); // Frees the list and all elements left in it (if any)
+}
